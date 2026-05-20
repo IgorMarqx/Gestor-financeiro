7c7969ab50ef30b5414a570c5079702be9d655b3
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2026-05-20 20:39:54 -0300

    adicionar métodos home e transacoesIndex ao WebController

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -3,14 +3,29 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class WebController extends Controller
 {
+    public function home(): RedirectResponse|Response
+    {
+        if (auth()->check()) {
+            return redirect()->route('dashboard');
+        }
+
+        return Inertia::render('auth/login');
+    }
+
     public function dashboard()
     {
         return Inertia::render('dashboard');
+    }
+
+    public function transacoesIndex()
+    {
+        return Inertia::render('transacoes/index');
     }
 
     public function chatIndex()
