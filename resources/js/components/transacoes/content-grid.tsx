27e54c37ef2edf7ac598c11e0676f8853bac25b1
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2026-05-21 17:55:46 -0300

    adicionar tipagem e propriedades ao componente ContentGrid

--- resources/js/components/transacoes/content-grid.tsx
+++ resources/js/components/transacoes/content-grid.tsx
@@ -1,11 +1,46 @@
+import { ApiConta } from '@/types/ApiConta';
+import { ApiTransacao } from '@/types/ApiTransacao';
 import SidePanel from './side-panel';
 import TransactionsTable from './transactions-table';
 
-export default function ContentGrid() {
+type ContentGridProps = {
+    contas: ApiConta[];
+    transacoes: ApiTransacao[];
+    isLoading: boolean;
+    monthBalance: string;
+    totalEntradas: string;
+    totalSaidas: string;
+    onCreateConta: () => void;
+    onEditTransacao: (transacao: ApiTransacao) => void;
+    onDeleteTransacao: (transacao: ApiTransacao) => void;
+};
+
+export default function ContentGrid({
+    contas,
+    transacoes,
+    isLoading,
+    monthBalance,
+    totalEntradas,
+    totalSaidas,
+    onCreateConta,
+    onEditTransacao,
+    onDeleteTransacao,
+}: ContentGridProps) {
     return (
         <div className="grid gap-[18px] xl:grid-cols-[minmax(0,1fr)_300px]">
-            <TransactionsTable />
-            <SidePanel />
+            <TransactionsTable
+                transacoes={transacoes}
+                isLoading={isLoading}
+                onEdit={onEditTransacao}
+                onDelete={onDeleteTransacao}
+            />
+            <SidePanel
+                contas={contas}
+                monthBalance={monthBalance}
+                totalEntradas={totalEntradas}
+                totalSaidas={totalSaidas}
+                onCreateConta={onCreateConta}
+            />
         </div>
     );
 }
